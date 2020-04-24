41a7a6585749bee403775322cd54a723342774f7
Samuel Carleial <samuel.carleial@gmail.com>
2020-04-24 15:22:20 +0200

    update random forest: add tree()

--- reference_guide/random_forest/random_forest.R
+++ reference_guide/random_forest/random_forest.R
@@ -7,6 +7,16 @@ library('caTools')
 library('randomForest')
 library('randomForestExplainer')
 
+# Regression tree
+library("tree")
+
+# Fit a classification or regression tree (tree() documentation)
+# A tree is grown by binary recursive partitioning using the response in the 
+# specified formula and choosing splits from the terms of the right-hand-side.
+see <- tree(curr_agg_re ~ ., data = df) 
+plot(see)
+summary(see)
+text(see)
 
 
 # NOTES:
